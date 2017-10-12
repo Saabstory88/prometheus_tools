baa50a87ea7028790ecc9e1b0d7a924274a4d9eb
Saabstory88 <pete@clearstorycreative.com>
2017-10-12 20:14:06 +0000

    Add upload scripts for bloomfield

---
+++ stack/upbf.sh
@@ -0,0 +1,10 @@
+echo "Uploading bloomfield stack"
+python stack_upload.py 10.1.10.20 laundry.bin
+echo "Uploaded to Laundry"
+python stack_upload.py 10.1.10.18 church.bin
+echo "Uploaded to Church"
+python stack_upload.py 10.1.10.23 katmandu.bin
+echo "Uplaoded to Katmandu"
+python stack_upload.py 10.1.10.10 bank.bin
+echo "Uploaded to Bank"
+
