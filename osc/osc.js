0e434d8419ec6399f7ed7f31c056d998a7ae6ad9
Saabstory88 <pete@clearstorycreative.com>
2017-11-13 17:29:02 -0500

    Ready for install test

--- osc/osc.js
+++ osc/osc.js
@@ -1,2 +1,27 @@
 const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
+const udp = dgram.createSocket('udp4');
+const osc = require('osc-min');
+
+
+
+udp.on('message', function(msg){
+  console.log(msg);
+})
+
+let msg = osc.toBuffer({
+  address: "/set/loadandgo",
+  args: [1, 1]
+});
+
+udp.bind(9000, '192.168.1.125');
+
+
+function test(){
+  udp.setBroadcast(true);
+  
+  udp.send(msg, 9000, "192.168.1.255", function(err){
+    if(err) { console.log(err) };
+  });
+}
+
+setTimeout(test, 1000);
